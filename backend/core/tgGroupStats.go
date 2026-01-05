389fd6ddc597fd7f523938359f92a0533a5f9a27
nrf24l01 <s.nevrovsky@ya.ru>
2026-01-05 23:34:32 +0300

    Add logic to prefix AllowedChatIDs with "-100" in LoadTGGroupStatsConfigFromEnv

--- backend/core/tgGroupStats.go
+++ backend/core/tgGroupStats.go
@@ -2,6 +2,8 @@ package core
 
 import (
 	"log"
+	"strconv"
+	"strings"
 
 	"github.com/caarlos0/env/v11"
 )
@@ -15,6 +17,17 @@ func LoadTGGroupStatsConfigFromEnv() *TGGroupStatsConfig {
 	config := &TGGroupStatsConfig{}
 	if err := env.Parse(config); err != nil {
 		log.Fatalf("Failed to parse environment variables: %v", err)
+	}
+	for i, id := range config.AllowedChatIDs {
+		idStr := strconv.FormatInt(id, 10)
+		if !strings.HasPrefix(idStr, "-100") {
+			newIdStr := "-100" + idStr
+			newId, err := strconv.ParseInt(newIdStr, 10, 64)
+			if err != nil {
+				log.Fatalf("Failed to parse prefixed chat id %q: %v", newIdStr, err)
+			}
+			config.AllowedChatIDs[i] = newId
+		}
 	}
 	return config
 }
